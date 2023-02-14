1a825a3b7d0f1d176b7df19cd12685b42f9b6029
khangn <khangnln42@gmail.com>
2023-02-14 09:55:25 +0700

    1010-State

--- firstapp/src/Xe.js
+++ firstapp/src/Xe.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 class Xe extends React.Component{
+    constructor(props){
+        console.log('Xe constuctor : ',props )
+        super(props);
+        this.state = {
+            color:this.props.color,
+            brand:this.props.brand,
+            value: this.props.value
+        }
+        
+    }
     render(){
         return (
             <div>
                 {/* <h2 style={{color: "red"}}>Hi, Toi la chiec xe đỏ {}</h2> */}
-                <h2 style={{color: this.props.color}}>Hi, Toi la chiec xe {this.props.value}</h2>
-                <h2 style={{color: this.props.color}}>Nhãn hiệu của tôi là {this.props.brand}</h2>
+                {/* <h2 style={{color: this.props.color}}>Hi, Toi la chiec xe {this.props.value}</h2> */}
+                {/* <h2 style={{color: this.props.color}}>Nhãn hiệu của tôi là {this.props.brand}</h2> */}
+                <h2 style={{color:this.state.color}}>Tôi là xe {this.state.brand} {this.state.value}</h2>
             </div>
        
        
